e3ca14f4574338c45ed6cd56f05ffce1d2920075
fruktlunch <adam.lannerholm@gmail.com>
2023-09-22 11:22:29 +0200

    SceneSetup
    -Lagt till dungeonscenen
    -Kodat triggerövergång från utomhusscen till dungeonscen
    -lagt in placeholder-bilder som bakgrunder för båda scenerna

---
+++ Assets/Scripts/LevelMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if(collision.gameObject.name == "Player")
+        {
+           
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+}
