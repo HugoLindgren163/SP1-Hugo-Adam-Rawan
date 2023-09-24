2b693983e43643654aca0984e710616a972cdd14
fruktlunch <adam.lannerholm@gmail.com>
2023-09-24 10:34:26 +0200

    Enemy patrol
    -Enemy movement för patrullerande fiender

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 2.0f;
+    private SpriteRenderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2 (moveSpeed, 0) * Time.deltaTime);
+
+        if(moveSpeed > 0 ) 
+        {
+            rend.flipX = false;
+        }
+        else
+        {
+            rend .flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyBlock"))
+        {
+            moveSpeed = -moveSpeed;
+        }
+
+    }
+}
