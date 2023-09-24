463615fdd82a28154d6ab8f2a70d963618f6ad31
fruktlunch <adam.lannerholm@gmail.com>
2023-09-24 10:00:37 +0200

    Groundtiles och Enemymovement
    -bytt ut groundtiles till hugos
    -kodat in enemyMovement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,6 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 2.0f;
+<<<<<<< Updated upstream
     [SerializeField] private int damageGiven = 1;
     private SpriteRenderer rend;
 
@@ -29,10 +30,35 @@ public class EnemyMovement : MonoBehaviour
         {
             rend.flipX = true;
         }
+=======
+    private SpriteRenderer rend;
+
+    
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();  
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2 (moveSpeed, 0) * Time.deltaTime);   
+
+        if(moveSpeed > 0 ) 
+        {
+            rend.flipX = false;
+        }
+        else
+        {
+            rend.flipX = true;
+        }
+
+>>>>>>> Stashed changes
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+<<<<<<< Updated upstream
         if(other.gameObject.CompareTag("EnemyBlock"))
         {
             moveSpeed = -moveSpeed;
@@ -50,3 +76,14 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 }
+=======
+        if (other.gameObject.CompareTag("EnemyBlock"))
+        {
+            moveSpeed = -moveSpeed;
+        }
+    }
+
+}
+
+
+>>>>>>> Stashed changes
